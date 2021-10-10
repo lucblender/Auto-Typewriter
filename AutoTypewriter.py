a1aa031a75fc963d80b6708a6c76441820c8a83b
lucblender <lucasbonvin@hotmail.com>
2021-10-10 17:38:37 +0200

    first commit

---
+++ AutoTypewriter.py
@@ -0,0 +1,75 @@
+import RPi.GPIO as GPIO
+from time import sleep, time
+from KeyDictionnary import keys_dict
+
+left_1 = 5
+left_2 = 6
+left_3 = 13
+left_4 = 19
+left_5 = 26
+left_6 = 16
+left_7 = 20
+left_8 = 21
+
+right_1 = 4
+right_2 = 17 
+right_3 = 18 
+right_4 = 27 
+right_5 = 22 
+right_6 = 24 
+right_7 = 23 
+right_8 = 25 
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(left_1, GPIO.IN)
+GPIO.setup(left_2, GPIO.IN)
+GPIO.setup(left_3, GPIO.IN)
+GPIO.setup(left_4, GPIO.IN)
+GPIO.setup(left_5, GPIO.IN)
+GPIO.setup(left_6, GPIO.IN)
+GPIO.setup(left_7, GPIO.IN)
+GPIO.setup(left_8, GPIO.IN)
+
+
+GPIO.setup(right_1, GPIO.OUT, initial=1)
+GPIO.setup(right_2, GPIO.OUT, initial=1)
+GPIO.setup(right_3, GPIO.OUT, initial=1)
+GPIO.setup(right_4, GPIO.OUT, initial=1)
+GPIO.setup(right_5, GPIO.OUT, initial=1)
+GPIO.setup(right_6, GPIO.OUT, initial=1)
+GPIO.setup(right_7, GPIO.OUT, initial=1)
+GPIO.setup(right_8, GPIO.OUT, initial=1)
+
+def press_key(keys):
+    emit, receive, shift_pressed = keys
+    if shift_pressed:
+        emit_shift, receive_shift, shift_pressed = keys_dict["shift"]        
+        before = time()
+        while(time()-before < 0.05):
+            GPIO.output(receive_shift, GPIO.input(emit_shift))            
+        before = time()
+        while(time()-before < 0.1):
+            GPIO.output(receive_shift, GPIO.input(emit_shift))
+            GPIO.output(receive, GPIO.input(emit))
+        GPIO.output(receive_shift, 1)
+        GPIO.output(receive, 1)
+    else:
+        before = time()
+        while(time()-before < 0.1):
+            GPIO.output(receive, GPIO.input(emit))
+        GPIO.output(receive, 1)
+    sleep(0.1)
+
+def press_string(string):
+    for char in string:
+        press_key(keys_dict[char])
+        
+        
+def test_string(string):
+    for char in string:
+        try:
+            keys_dict[char]
+        except:
+            print("error, key not in dict: ", char)
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Auto-Typewriter
+# Auto-Typewriter
+
+
+## Ascii art example
+press_string("                i iii iiii   i   i    i\n     rv    rbBBBBBiBBBBBBi   iri ijvivBBMRiiDBLv\n   BBQBBBBBBBbidBQ  Bqiiv   uDQBQgBZBBBQBBBBBQBBBBBBBY\n   i  BQQQMgQr ZBB       JDrQUrBBBQBgDZDdZRjiggBUv sB\n      BDDEgMJBBBPM        ZQQBBRvIKrgDZEZEMIuDgZBBi\n     BBMDBBBQQ           uQ  vvLivR YQDDEDEgggZQPPiB\n     iBQBJiXi            ZBQBiivrBBQQBBBQgQQMDQQu ii\n      KBQ   v          UBBgMMBBBB jBBMiiQBQdBBDRr\n        ibBu YU        gBQBQMZgDQb Ki   rQ  jBd  ki\n          vvuBQBi       dBBQBQZggBBQ     u   B  viZ\n           iBBQQBPr         JQgZMQS          EvbBPiiqJ\n           iBBgggBBBi        BMgMB             v i  iB\n             vBDgQBs         BBRBviBi             gBdg\n              BQQBi          uBBB  Q            BBQBBBQ\n              BBBi            Du                gIJgBQi  r\n              QB                                   vQ  ivk\n              iB  Ii                                   i\n                                         ivrriiriiiiiiiiirri\n                                          i    i         ii\n")
