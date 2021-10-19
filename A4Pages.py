ca554c333ddf9a5bd6746e1a76f2b5bffd3a3b19
lucblender <lucasbonvin@hotmail.com>
2021-10-19 22:35:53 +0200

    add begining of A4 pages design class

---
+++ A4Pages.py
@@ -0,0 +1,42 @@
+import copy
+import tabulate
+class A4Page:
+    
+    column = 65
+    line = 90
+    
+    def __init__(self):
+        self.page = [[" "]*self.column for i in range(self.line)]
+        
+    def reset_page(self):
+        self.page = [[" "]*self.column for i in range(self.line)]
+        
+    def insert_text(self, text, x, y):
+        line_text  = text.splitlines() 
+        index_y = 0
+        for line in line_text:             
+            index_x  = 0
+            for char in line:
+                self.page[index_y+y][index_x+x] = char
+                index_x += 1
+            index_y += 1
+            
+    def insert_line(self, char, y):
+        for i in range(0, self.column):
+            self.page[y][i] = char
+    
+    def insert_table
+            
+    def __repr__(self):
+        temp_page = copy.deepcopy(self.page)
+        
+        while(temp_page[-1] == [" "]*self.column):
+            temp_page.pop()
+        
+        to_return = ""
+        for char_line in temp_page:
+            line = ""
+            for char in char_line:
+                line += char
+            to_return += (line.rstrip()+"\n")
+        return to_return
