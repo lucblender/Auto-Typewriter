4664dae40d69d3b725670989e9fa60fa68426e4b
lucblender <lucasbonvin@hotmail.com>
2021-10-27 17:26:30 +0200

    Add agenda service

---
+++ GCalendarEvents.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+from datetime import datetime
+from datetime import timedelta  
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from columnar import columnar
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def getEvents():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('C:/Users/LucasBonvin/Documents/GitRandom/Auto-Typewriter/token.json'):
+        creds = Credentials.from_authorized_user_file('C:/Users/LucasBonvin/Documents/GitRandom/Auto-Typewriter/token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'C:/Users/LucasBonvin/Documents/GitRandom/Auto-Typewriter/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080)
+        # Save the credentials for the next run
+        with open('C:/Users/LucasBonvin/Documents/GitRandom/Auto-Typewriter/token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    today  =datetime.today()
+    monday = today - timedelta(days=today.weekday())
+    sunday = monday + timedelta(days=6)
+
+    today_str = datetime(monday.year, monday.month, monday.day, hour = 0, minute=0).isoformat() + 'Z'
+    sunday_str =  datetime(sunday.year, sunday.month, sunday.day, hour = 23, minute=59).isoformat() + 'Z'
+
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=today_str,
+                                        timeMax=sunday_str,
+                                        maxResults=100, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        return []
+    else:
+        return events
+
+    
+    
---
+++ TypeAgendaService.py
@@ -0,0 +1,74 @@
+
+from PIL import Image
+
+from GCalendarEvents import getEvents
+
+from datetime import datetime
+from datetime import timedelta  
+
+from columnar import columnar
+
+from A4Pages import A4Page
+
+from AsciiGenerator import convertImageToAscii
+from KeyDictionnary import *
+
+page = A4Page()
+
+events = getEvents()
+headers  = []
+today  =datetime.today()
+monday = today - timedelta(days=today.weekday())
+sunday = monday+ timedelta(days=6)
+day = monday
+for i in range(0,6):
+    headers.append(day.strftime("%a\n%d"))
+    day = day+ timedelta(days=1)
+
+data = [[""]*7 for i in range(24)]
+
+for event in events:
+    start = event['start'].get('dateTime', event['start'].get('date'))
+    start_datetime = datetime.fromisoformat(start)
+    end = event['end'].get('dateTime', event['end'].get('date'))
+    end_datetime = datetime.fromisoformat(start)    
+    day_index = start_datetime.weekday()
+    hour_index = start_datetime.hour 
+    event_text = event['summary']
+    event_time = start_datetime.strftime("%H:%M") #+"\n"+end_datetime.strftime("%H:%M")
+    if data[hour_index][day_index] != "":
+        data[hour_index][day_index]+= "========\n"
+    data[hour_index][day_index]+=(event_time+"\n"+event_text+"\n")
+    
+while(data[0] == ['']*7):
+    data.pop(0)
+    
+while(data[-1] == ['']*7):
+    data.pop()
+
+
+table = columnar(data, headers, min_column_width=9, max_column_width=9, terminal_width=65,column_sep='/')
+
+
+img = Image.open("Calendar_icon.png")
+ascii_img = convertImageToAscii(img, 30, 0.5, False)
+page.insert_text(ascii_img, 30, 1)
+page.insert_line("=", 0)
+page.insert_line("_", 16)
+page.insert_text("Calendar of week "+str(monday.isocalendar().week), 3, 4)
+page.insert_text("From "+monday.strftime("%a, the %d %b %Y"), 3, 7)
+page.insert_text("To   "+sunday.strftime("%a, the %d %b %Y"), 3, 8)
+page.insert_text("_"*25, 3, 10)
+page.insert_text(str(len(events))+ " event(s) this week", 3, 13)
+
+page.insert_text(table, 2, 18)
+
+split_table = table.splitlines()
+
+
+page.insert_line("=", 18+len(split_table)+2)
+
+test_string(str(page))
+
+print(str(page))
+
